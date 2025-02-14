84a7a31feddfd2d2bc5e34e49141e7dd497cec99
ashraful-fahim <ashraful30june@gmail.com>
2025-02-15 02:37:53 +0600

    completed the first function practice task

---
+++ task-1.js
@@ -0,0 +1,13 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1, num2, num3, num4) {
+    const multi = num1 * num2 * num3 * num4;
+    return multi; 
+}
+
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+const num4 = 4;
+const multi = multiply(num1, num2, num3, num4);
+console.log(multi);
