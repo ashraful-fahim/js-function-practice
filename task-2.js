1ded8c969905f1ebddb7c6dc0f4063d3ecfa5338
ashraful-fahim <ashraful30june@gmail.com>
2025-02-15 03:51:09 +0600

    completed the task with odd and even

--- task-2.js
+++ task-2.js
@@ -1,9 +1,11 @@
-function numConditioned(num){
-    if(num % 2 === 1){
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function numConditioned(num) {
+    if (num % 2 === 1) {
         const multi = num * 2;
         return multi;
     }
-    else{
+    else {
         const divide = num / 2;
         return divide;
     }
